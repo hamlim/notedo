b67d5ca8a979c43ce1ca4a236ff7a656cb718d4e
Matt Hamlin <matthewjameshamlin@gmail.com>
2020-06-19 17:21:58 -0400

    Add WIP details, bold, and italics parsing

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import {
   Box,
   Textarea,
   Text,
-  HiddenCheckbox,
+  Checkbox,
   Label,
   Button,
   H1,
@@ -17,83 +17,160 @@ let { useState, Fragment, useReducer } = React
 function ControlledCheckbox({ label, checked, onChange }) {
   return (
     <Label display="inline-flex">
-      <HiddenCheckbox checked={checked} onChange={onChange} />
+      <Checkbox checked={checked} onChange={onChange} />
       {label}
     </Label>
   )
 }
 
-// minimal
-// [] todo item
-// [x] todo item
-// classic
+let boldRegex = new RegExp('\\*\\*(.+)\\*\\*')
+let italicsRegex = new RegExp('__(.+)__')
+
+// Supported Format:
 // [ ] todo item
-// [X] todo item
+// [x] todo item
 
-function isLineMinimalEmptyCheckbox(line) {
-  return line.trim().startsWith('[]')
-}
-
-function isLineClassicEmptyCheckbox(line) {
+function isLineEmptyCheckbox(line) {
   return line.trim().startsWith('[ ]')
 }
 
-function isLineEmptyCheckbox(line) {
-  return isLineMinimalEmptyCheckbox(line) || isLineClassicEmptyCheckbox(line)
-}
-
-function isLineMinimalCheckedCheckbox(line) {
+function isLineCheckedCheckbox(line) {
   return line.trim().startsWith('[x]')
 }
 
-function isLineClassicCheckedCheckbox(line) {
-  return line.trim().startsWith('[X]')
-}
+function Lines({ lines, dispatch, settings }) {
+  let content = []
 
-function isLineCheckedCheckbox(line) {
-  return (
-    isLineMinimalCheckedCheckbox(line) || isLineClassicCheckedCheckbox(line)
-  )
-}
-
-function Line({ line, lineNum, dispatch, settings }) {
-  let content
-  if (line.startsWith('//')) {
-    // ignore comments
-    return null
-  } else if (isLineEmptyCheckbox(line) || isLineCheckedCheckbox(line)) {
-    let checked = false
-    if (isLineCheckedCheckbox(line)) {
-      checked = true
+  let isInDetails = false
+  let detailsLineNum = null
+  let details = []
+  for (let idx in lines) {
+    let lineNum = Number(idx) + 1
+    let line = lines[idx]
+    if (line.startsWith('>>')) {
+      isInDetails = true
+      detailsLineNum = lineNum
+      details.push({
+        summary: line.replace('>> ', ''),
+        content: [],
+      })
+    } else if (isInDetails && line.startsWith('  ')) {
+      details[details.length - 1].content.push({
+        content: <Text forwardedAs="span">{line}</Text>,
+        lineNum,
+      })
+    } else if (isInDetails && !line.startsWith('  ')) {
+      isInDetails = false
+      content.push({
+        isDetails: true,
+        content: details[details.length - 1],
+        lineNum: detailsLineNum,
+      })
+      // also push any content in the line
+      content.push({
+        content: <Text forwardedAs="span">{line}</Text>,
+        lineNum,
+      })
+    } else if (line.startsWith('//')) {
+      // ignore comments
+    } else if (isLineEmptyCheckbox(line) || isLineCheckedCheckbox(line)) {
+      let checked = false
+      if (isLineCheckedCheckbox(line)) {
+        checked = true
+      }
+      content.push({
+        content: (
+          <ControlledCheckbox
+            checked={checked}
+            onChange={() =>
+              dispatch({
+                type: 'toggle-todo',
+                lineNum,
+              })
+            }
+            label={line.replace('[ ]', '').replace('[x]', '')}
+          />
+        ),
+        lineNum,
+      })
+    } else if (boldRegex.test(line)) {
+      let { 1: boldedContent, index } = line.match(boldRegex)
+      let preBold = line.slice(0, index)
+      let postBold = line.slice(index + boldedContent.length + 4)
+      content.push({
+        content: (
+          <Fragment>
+            {preBold.length && <Text forwardedAs="span">{preBold}</Text>}
+            <Text forwardedAs="strong" fontWeight="bold">
+              {boldedContent}
+            </Text>
+            {postBold.length && <Text forwardedAs="span">{postBold}</Text>}
+          </Fragment>
+        ),
+        lineNum,
+      })
+    } else if (italicsRegex.test(line)) {
+      let { 1: italicContent, index } = line.match(italicsRegex)
+      let preItalics = line.slice(0, index)
+      let postItalics = line.slice(index + italicContent.length + 4)
+      content.push({
+        content: (
+          <Fragment>
+            {preItalics.length && <Text forwardedAs="span">{preItalics}</Text>}
+            <Text forwardedAs="em" fontStyle="italics">
+              {italicContent}
+            </Text>
+            {postItalics.length && (
+              <Text forwardedAs="span">{postItalics}</Text>
+            )}
+          </Fragment>
+        ),
+        lineNum,
+      })
+    } else {
+      // @TODO handle links, bold, emphasis, tags, metadata
+      content.push({
+        content: <Text forwardedAs="span">{line}</Text>,
+        lineNum,
+      })
     }
-    content = (
-      <ControlledCheckbox
-        checked={checked}
-        onChange={(checked) =>
-          dispatch({
-            type: 'toggle-todo',
-            lineNum,
-          })
-        }
-        label={line}
-      />
-    )
-  } else {
-    // @TODO handle links, bold, emphasis, tags, metadata
-    content = <Text forwardedAs="span">{line}</Text>
   }
-  return (
-    <Fragment>
-      {settings?.showLineNums ? (
-        <Text forwardedAs="span" color="gray.3">
-          {lineNum}
-        </Text>
-      ) : null}
+  return content.map(({ content, lineNum, isDetails = false }) => {
+    if (isDetails) {
+      return (
+        <Fragment key={lineNum}>
+          <details>
+            <summary>{content.summary}</summary>
+            {content.content.map(({ content, lineNum }) => (
+              <Fragment key={lineNum}>
+                {settings?.showLineNums ? (
+                  <Text forwardedAs="span" color="gray.3">
+                    {lineNum}
+                  </Text>
+                ) : null}
 
-      {content}
-      <br />
-    </Fragment>
-  )
+                {content}
+                <br />
+              </Fragment>
+            ))}
+          </details>
+          <br />
+        </Fragment>
+      )
+    }
+    return (
+      <Fragment key={lineNum}>
+        {settings?.showLineNums ? (
+          <Text forwardedAs="span" color="gray.3">
+            {lineNum}
+          </Text>
+        ) : null}
+
+        {content}
+        <br />
+      </Fragment>
+    )
+  })
 }
 
 function reducer(state, action) {
@@ -106,21 +183,11 @@ function reducer(state, action) {
           .map((line, idx) => {
             if (idx === lineNum - 1) {
               if (isLineEmptyCheckbox(line)) {
-                // [] todo item
-                if (isLineMinimalEmptyCheckbox(line)) {
-                  return line.replace(/\[\]/, '[x]')
-                } else {
-                  // [ ] todo item
-                  return line.replace(/\[ \]/, '[x]')
-                }
+                // [ ] todo item
+                return line.replace(/\[ \]/, '[x]')
               } else if (isLineCheckedCheckbox(line)) {
                 // [x] todo item
-                if (isLineMinimalCheckedCheckbox(line)) {
-                  return line.replace(/\[\x]/, '[]')
-                } else {
-                  // [X] todo item
-                  return line.replace(/\[X\]/, '[]')
-                }
+                return line.replace(/\[\x]/, '[ ]')
               }
             }
             return line
@@ -169,23 +236,14 @@ export default function Notedo() {
       </Box>
       <Box>
         <Textarea
+          fontFamily="monospace"
           value={state.value}
           onChange={(val) => dispatch({ type: 'change', value: val })}
           minHeight="calc(100vh - var(--header-height))"
         />
       </Box>
       <Box forwardedAs="pre">
-        {lines.map((line, idx) => {
-          return (
-            <Line
-              settings={settings}
-              line={line}
-              key={idx}
-              lineNum={idx + 1}
-              dispatch={dispatch}
-            />
-          )
-        })}
+        <Lines lines={lines} settings={settings} dispatch={dispatch} />
       </Box>
     </Box>
   )
